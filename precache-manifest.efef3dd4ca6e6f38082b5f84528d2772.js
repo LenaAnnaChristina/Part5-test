b994248cd535c506cf2ebf748a40966921d74890
Lena Nyman-Lehtinen <92268539+LenaAnnaChristina@users.noreply.github.com>
2022-06-06 20:10:17 +0300

    Updates

---
+++ precache-manifest.efef3dd4ca6e6f38082b5f84528d2772.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6381e10e8ceec524b5ea",
+    "url": "/Part5-test/static/js/main.ce56c7b0.chunk.js"
+  },
+  {
+    "revision": "bbef5225e66ccb403636",
+    "url": "/Part5-test/static/js/runtime~main.2a344710.js"
+  },
+  {
+    "revision": "9b9eedcde606e4116d8f",
+    "url": "/Part5-test/static/js/2.51a98743.chunk.js"
+  },
+  {
+    "revision": "b2ad2c69671c53c917bdec620629cc5c",
+    "url": "/Part5-test/index.html"
+  }
+];
